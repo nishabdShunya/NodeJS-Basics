1d2f7d18b682200cbfab51ebab70ae10e3cfce37
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-18 12:13:05 +0530

    connecting request with response, basically routing requests

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ const fs = require('fs');
 
 // Creating server and storing it in a variable
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.write(` <html>
+                    <head>
+                        <title>Message Page</title>
+                    </head>
+                    <body>
+                        <form action="/message" method="POST">
+                            <input type="text" name="message">
+                            <button type="submit">SEND</button>
+                        </form>
+                    </body>
+                </html>`);
+        return res.end();  // response ends here    
+        // Here, return is important because we want to quit out of the function if we have entered the "if" block.
+    }
+
     // Filling the response object with some data
     res.setHeader('Content-Type', 'text/html'); // NOTE: Content-Type is a default header that browser understands
     res.write(` <html>

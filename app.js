22a2406c7e41b33d62d24fd1a47d151d992a9a66
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-21 01:42:07 +0530

    cleaned up the code using routers and send 404 Page Not Found error for non-existent routes, plus added filters to the routes

--- app.js
+++ app.js
@@ -3,23 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    res.send(`  <form action="/product" method="POST">
-                    <input type="text" name="title" placeholder="product">
-                    <input type="number" name="size" placeholder="size"> 
-                    <button type="submit">Add Product</button>
-                </form>`);
-});
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Welcome Home</h1>');
-});
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+})
 
 app.listen(3000);

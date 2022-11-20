02be539d5f4cc640c3f341b2b3f1f83541c7dcb0
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-21 00:59:15 +0530

    using body parser with the middlewares

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Inside a Middleware.');
-    next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`  <form action="/product" method="POST">
+                    <input type="text" name="title" placeholder="product">
+                    <input type="number" name="size" placeholder="size"> 
+                    <button type="submit">Add Product</button>
+                </form>`);
 });
 
-app.use((req, res, next) => {
-    console.log('Inside Another Middleware.');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Welcome Home</h1>');
 });
 
